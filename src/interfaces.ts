52159701470b9cbfa6abbc862bf1e422689b1e0f
Bhaag Marway <bsmarway@gmail.com>
2023-09-02 10:01:50 -0400

    Added interface for Robot and House

---
+++ src/interfaces.ts
@@ -0,0 +1,12 @@
+export interface Robot {
+    x: number;
+    y: number;
+    presentsDelivered: number;
+    name: string;
+}
+
+export interface House {
+    x: number;
+    y: number;
+    numPresents: number;
+}
